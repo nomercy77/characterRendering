3f3b85226d75710934132776cc93080acc665f7a
Amit Singh <42760910+nomercy77@users.noreply.github.com>
2022-07-22 12:01:53 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
   const [placeholder, setPlaceholder] = React.useState("");
 
   const string =
-      "M y output is better this way. This is a better UX for caption generation!!",
+      "M y output is better this way. This is a better UX for caption generation! Look, sampled output: I know its hard keeping up with your bodily needs and conditions every time. My WhatsApp Clinic aims at doing that for you, keeping an eye out for you. Just text my Digital Clinic to help you sort out your medical woes!",
     index = React.useRef(0);
 
   React.useEffect(() => {
@@ -16,7 +16,7 @@ export default function App() {
       index.current++;
     }
     if (index.current < string.length - 1) {
-      let addChar = setInterval(update, 50);
+      let addChar = setInterval(update, 40);
       return () => clearInterval(addChar);
     }
   }, [placeholder]);
