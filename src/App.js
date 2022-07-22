f0b628ee185818ed61a055d2e9d1ab6dd8e41bde
Amit Singh <42760910+nomercy77@users.noreply.github.com>
2022-07-22 11:57:43 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom/client";
 // import ReactGA from "react-ga4"
 // react library for routing
-import { Route, Switch, Redirect, useLocation } from "react-router-dom";
+import { Route } from "react-router-dom";
 export default function App() {
   const [placeholder, setPlaceholder] = React.useState("");
 
