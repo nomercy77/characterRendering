e7cba61d7a9c5b260c9d9db39e6e6b97632354f1
Amit Singh <42760910+nomercy77@users.noreply.github.com>
2022-07-22 12:17:01 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -4,16 +4,15 @@ import ReactDOM from "react-dom/client";
 // react library for routing
 import { Route } from "react-router-dom";
 export default function App() {
-  const [placeholder, setPlaceholder] = React.useState("");
-
   const string =
-      "M y output is better this way. This is a better UX for caption generation! Look, sampled output: I know its hard keeping up with your bodily needs and conditions every time. My WhatsApp Clinic aims at doing that for you, keeping an eye out for you. Just text my Digital Clinic to help you sort out your medical woes!",
+      "My output is better this way. This is a better UX for caption generation! Look, sampled output: I know its hard keeping up with your bodily needs and conditions every time. My WhatsApp Clinic aims at doing that for you, keeping an eye out for you. Just text my Digital Clinic to help you sort out your medical woes!",
     index = React.useRef(0);
+  const [placeholder, setPlaceholder] = React.useState(string[0]);
 
   React.useEffect(() => {
     function update() {
-      setPlaceholder((prev) => prev + string[index.current]);
       index.current++;
+      setPlaceholder((prev) => prev + string[index.current]);
     }
     if (index.current < string.length - 1) {
       let addChar = setInterval(update, 40);
