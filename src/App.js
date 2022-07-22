fa1a1f6a61b719c79f0bce92dcce1294666641b5
Amit Singh <42760910+nomercy77@users.noreply.github.com>
2022-07-22 12:45:30 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ReactDOM from "react-dom/client";
 import { Route } from "react-router-dom";
 export default function App() {
   const string =
-      "My output is better this way. This is a better UX for caption generation! Look, sampled output: I know its hard keeping up with your bodily needs and conditions every time. My WhatsApp Clinic aims at doing that for you, keeping an eye out for you. Just text my Digital Clinic to help you sort out your medical woes!",
+      "I know its hard keeping up with your bodily needs and conditions every time. My WhatsApp Clinic aims at doing that for you, keeping an eye out for you. Just text my Digital Clinic to help you sort out your medical woes!",
     index = React.useRef(0);
   const [placeholder, setPlaceholder] = React.useState(string[0]);
 
@@ -15,7 +15,7 @@ export default function App() {
       setPlaceholder((prev) => prev + string[index.current]);
     }
     if (index.current < string.length - 1) {
-      let addChar = setInterval(update, 40);
+      let addChar = setInterval(update, 20);
       return () => clearInterval(addChar);
     }
   }, [placeholder]);
